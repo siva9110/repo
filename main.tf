22c2d6f54cd57cd32e72eb96b91d5f727a1f4376
siva9110 <golisiva50@gmail.com>
2025-10-26 11:20:19 +0530

    fucking

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
 
 resource "azurerm_virtual_network" "v-net" {
   name                = var.azurerm_virtual_network
-  address_space       = ["10.0.100.0/16"]
+  address_space       = ["10.0.0.0/16"]
   location            = var.azurerm_location
   resource_group_name = var.azurerm_resource_group
 
