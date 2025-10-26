d1d913d756a3fb9e6bf5227650eba5afda4e1180
siva9110 <golisiva50@gmail.com>
2025-10-26 13:04:58 +0530

    real1

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ terraform {
 
 resource "azurerm_virtual_network" "v-net" {
   name                = var.azurerm_virtual_network
-  address_space       = ["10.0.0.0/24"]
+  address_space       = ["10.0.0.0/16"]
   location            = var.azurerm_location
   resource_group_name = var.azurerm_resource_group
 
@@ -33,7 +33,7 @@ resource "azurerm_subnet" "subnet-1" {
   name                 = "testingvsub"
   virtual_network_name = var.azurerm_virtual_network
   resource_group_name  = var.azurerm_resource_group
-  address_prefixes     = ["10.0.0.1/29"]
+  address_prefixes     = ["10.0.0.1/24"]
   depends_on           = [azurerm_virtual_network.v-net]
 
 }
